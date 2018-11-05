601ce6cbb3ba88773361431464d8dbc8afc7be8d
Jaime Lopez <loama18@gmail.com>
2018-11-05 04:03:48 -0600

    init vue project with travis

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <img src="./assets/map.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="sass">
+  #app
+    font-family: 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing: antialiased
+    -moz-osx-font-smoothing: grayscale
+    text-align: center
+    color: #2c3e50
+    margin-top: 60px
+    background: #fefefe
+
+  img
+    position: fixed
+    bottom: 0
+    left: 0
+</style>
---
+++ src/components/search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <img src="../assets/modus-text.svg">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+  img
+    position: absolute
+    top: 20px
+    left: calc(50vw - 100px)
+    height: 200px
+    width: 200px
+</style>
